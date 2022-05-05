a0efc8fbeda7d4375b5d6227629d585a2d4beedc
秋雨落 <i@rain.cx>
2022-05-05 13:32:06 +0800

    feat: 1.0.0
    Signed-off-by: 秋雨落 <i@rain.cx>

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A fabric mod with some little mechanisms for MeowCraft V4.
 ## Contents and Todos
 - [x] Add right click harvest for crops.
 - [x] Use bone meals to sugar cane.
+- [x] Animals will run away from the player who hurt their species nearby.
 - [ ] Eating watermelon slice with melon seed.
-- [ ] Animals will run away from the player who hurt their species nearby.
 - [ ] Drop all blocks which was broken in a creepers explode.
 - [ ] Configuration support.
--- src/main/java/al/yn/meowmechanism/MeowMechanism.java
+++ src/main/java/al/yn/meowmechanism/MeowMechanism.java
@@ -5,10 +5,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class MeowMechanism implements DedicatedServerModInitializer {
-    private Logger logger = LogManager.getLogger("MeowMechanism");
+    public static final String MOD_ID = "meowmechanism";
+    public static final String NAME = "MeowMechanism";
+    public static final String VERSION = "1.0.0";
+
+    private Logger logger = LogManager.getLogger(NAME);
 
     @Override
     public void onInitializeServer() {
-        logger.info("MeowMechanism is running!");
+        logger.info("MeowMechanism " + VERSION + " is running!");
     }
 }
