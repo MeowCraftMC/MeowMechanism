78da990012e20ac2ec940d2df3d8bd7af81ff653
秋雨落 <i@rain.cx>
2022-05-09 21:49:26 +0800

    fix: build.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/al/yn/meowmechanism/MeowMechanism.java
+++ src/main/java/al/yn/meowmechanism/MeowMechanism.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 public class MeowMechanism implements DedicatedServerModInitializer {
     public static final String MOD_ID = "meowmechanism";
     public static final String NAME = "MeowMechanism";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     private Logger logger = LogManager.getLogger(NAME);
 
